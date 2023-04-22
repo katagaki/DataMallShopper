61747a75351bf66bb3f77d8ea4197038eab11bb9
katagaki <68231035+katagaki@users.noreply.github.com>
2023-04-22 12:13:53 +0800

    Initial Commit

---
+++ DataMall Shopper/DataMall_ShopperApp.swift
@@ -0,0 +1,17 @@
+//
+//  DataMall_ShopperApp.swift
+//  DataMall Shopper
+//
+//  Created by 堅書 on 2023/04/22.
+//
+
+import SwiftUI
+
+@main
+struct DataMall_ShopperApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
